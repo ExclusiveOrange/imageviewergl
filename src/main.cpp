f4bda695ed3d62087218b501410091350e987dbc
Atlee Brink <ExclusiveOrange@users.noreply.github.com>
2021-08-22 00:34:16 -0700

    wrote struct Destroyer to manage lifetimes; refactored main(..) to use Destroyer so it can be flat instead of nested if's

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <stb_image.h>
 
 #include <cassert>
+#include <functional>
 #include <iostream>
 #include <optional>
 
@@ -36,146 +37,173 @@ loadShader( const char *filename, GLenum shaderType )
 
   GLint maxLength = 0;
   glGetShaderiv( shader, GL_INFO_LOG_LENGTH, &maxLength );
-
   // The maxLength includes the NULL character
   std::vector< GLchar > errorLog( maxLength );
   glGetShaderInfoLog( shader, maxLength, &maxLength, &errorLog[0] );
-
   std::cout << "bad shader (" << filename << ") " << errorLog.data() << std::endl;
+
+  glDeleteShader( shader );
 
   return {};
 }
+
+struct Destroyer
+{
+  const std::function< void( void ) > fnOnDestroy;
+  ~Destroyer() { fnOnDestroy(); }
+};
 } // namespace
 
 //==============================================================================
 
-int main()
+int main( int arc, char *argv[] )
 {
-  if( glfwInit())
-  {
-    glfwSetErrorCallback( glfwErrorCallback );
+  // TODO: check args: expect 1 filename
+  // TODO: fork threads:
+  //    A: try to load filename as image then join B
+  //    B: initialize glfw and create a window then wait for A
+  // TODO: if error in A then when A joins B, display message and quit
+  // TODO: else if no error (image loaded into CPU memory) then:
+  //    * load image into GPU memory (GL texture)
+  //    * change window size to fit image
+  //    * draw image initially, then as needed
 
-    struct { int hint, value; } windowHints[]{
-        { GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE },
-        { GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE },
-//        { GLFW_RESIZABLE, GL_FALSE },
-        { GLFW_CONTEXT_VERSION_MAJOR, 4 },
-        { GLFW_CONTEXT_VERSION_MINOR, 1 }
-    };
-
-    for( auto windowHint : windowHints )
-      glfwWindowHint( windowHint.hint, windowHint.value );
-
-    if( GLFWwindow *window = glfwCreateWindow( 640, 480, "My Title", nullptr, nullptr ))
-    {
-      glfwMakeContextCurrent( window );
-
-      // initialize glew
-      {
-
-        GLenum error = glewInit();
-        if( error != GLEW_OK )
-          std::cerr << "glewInit() failed" << std::endl;
-      }
-
-      // create texture for testing
-      GLuint texture{};
-      glGenTextures( 1, &texture );
-      glBindTexture( GL_TEXTURE_2D, texture );
-      {
-        int width, height, nChannels;
-        stbi_uc *image = stbi_load( "../texture.jpg", &width, &height, &nChannels, 0 );
-        // TODO: error check result of stbi_load
-        assert( nChannels > 0 && nChannels <= 4 );
-
-        GLenum formatByNumChannels[4]{
-            GL_RED,
-            GL_RG,
-            GL_RGB,
-            GL_RGBA
-        };
-        GLenum format = formatByNumChannels[nChannels - 1];
-
-        glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, format, GL_UNSIGNED_BYTE, image );
-
-        if( nChannels < 3 )
-        {
-          GLint swizzleMask[2][4]{
-              { GL_RED, GL_RED, GL_RED, GL_ONE },
-              { GL_RED, GL_RED, GL_RED, GL_GREEN }};
-
-          glTexParameteriv( GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask[nChannels-1] );
-        }
-
-        stbi_image_free( image );
-      }
-
-      if( std::optional< GLuint > maybeVertShader = loadShader( "../shaders/texture.vert", GL_VERTEX_SHADER ))
-      {
-        GLuint vertShader = *maybeVertShader;
-
-        if( std::optional< GLuint > maybeFragShader = loadShader( "../shaders/texture.frag", GL_FRAGMENT_SHADER ))
-        {
-          GLuint fragShader = *maybeFragShader;
-
-          GLuint shaderProgram = glCreateProgram();
-          glAttachShader( shaderProgram, vertShader );
-          glAttachShader( shaderProgram, fragShader );
-          glLinkProgram( shaderProgram );
-          GLint linkStatus;
-          glGetProgramiv( shaderProgram, GL_LINK_STATUS, &linkStatus );
-          if( linkStatus == GL_FALSE )
-          {
-            std::cerr << "shader program link failed!\n";
-            // TODO: refactor to handle this error; probably should do the shaders and link the program all in one, separate function
-          }
-
-          glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
-          glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
-          glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
-          glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-
-          glDisable( GL_CULL_FACE );
-          glUseProgram( shaderProgram );
-
-          // get error 1282 from glDrawArrays when I don't use any vertex array objects...
-          // shouldn't need any because the vertices are generated in the vert shader, but maybe I need something here...
-          GLuint vao = 0;
-          glGenVertexArrays( 1, &vao );
-          glBindVertexArray( vao );
-
-          // Event loop
-          while( !glfwWindowShouldClose( window ))
-          {
-            // Clear the screen to black
-//            glClearColor( 0.5f, 0.0f, 0.0f, 1.0f );
-//            glClear( GL_COLOR_BUFFER_BIT );
-
-            glDrawArrays( GL_TRIANGLE_STRIP, 0, 4 );
-
-            glfwSwapBuffers( window );
-            glfwPollEvents();
-          }
-
-          glDeleteProgram( shaderProgram );
-
-          glDeleteShader( fragShader );
-        }
-
-        glDeleteShader( vertShader );
-      }
-
-      // destroy texture
-      glDeleteTextures( 1, &texture );
-    }
-    else
-      std::cerr << "glfwCreateWindow(..) failed" << std::endl;
-
-    glfwTerminate();
-  }
-  else
+  if( !glfwInit())
   {
     std::cerr << "glfwInit() failed" << std::endl;
     return 1;
+  }
+  Destroyer _glfwInit{ glfwTerminate };
+
+  //------------------------------------------------------------------------------
+
+  glfwSetErrorCallback( glfwErrorCallback );
+
+  //------------------------------------------------------------------------------
+
+  for( auto[hint, value] : (int[][2]){
+      { GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE },
+      { GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE },
+      { GLFW_CONTEXT_VERSION_MAJOR, 4 },
+      { GLFW_CONTEXT_VERSION_MINOR, 1 }} )
+    glfwWindowHint( hint, value );
+
+  GLFWwindow *window = glfwCreateWindow( 640, 480, "My Title", nullptr, nullptr );
+  if( !window )
+  {
+    std::cerr << "glfwCreateWindow(..) failed" << std::endl;
+    return 1;
+  }
+  Destroyer _glfwCreateWindow{ [window] { glfwDestroyWindow( window ); }};
+
+  //------------------------------------------------------------------------------
+
+  glfwMakeContextCurrent( window );
+
+  //------------------------------------------------------------------------------
+
+  if( GLEW_OK != glewInit())
+  {
+    std::cerr << "glewInit() failed" << std::endl;
+    return 1;
+  }
+
+  //------------------------------------------------------------------------------
+
+  GLuint texture{};
+  glGenTextures( 1, &texture );
+  glBindTexture( GL_TEXTURE_2D, texture );
+  {
+    int width, height, nChannels;
+    stbi_uc *image = stbi_load( "../texture.jpg", &width, &height, &nChannels, 0 );
+    // TODO: error check result of stbi_load
+    assert( nChannels > 0 && nChannels <= 4 );
+
+    GLenum formatByNumChannels[4]{
+        GL_RED,
+        GL_RG,
+        GL_RGB,
+        GL_RGBA
+    };
+    GLenum format = formatByNumChannels[nChannels - 1];
+
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, format, GL_UNSIGNED_BYTE, image );
+
+    if( nChannels < 3 )
+    {
+      GLint swizzleMask[2][4]{
+          { GL_RED, GL_RED, GL_RED, GL_ONE },
+          { GL_RED, GL_RED, GL_RED, GL_GREEN }};
+
+      glTexParameteriv( GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_RGBA, swizzleMask[nChannels - 1] );
+    }
+
+    stbi_image_free( image );
+  }
+  Destroyer _texture{ [&texture]{ glDeleteTextures(1, &texture); }};
+
+  //------------------------------------------------------------------------------
+
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+  glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+  //------------------------------------------------------------------------------
+
+  GLuint vertShader{};
+  if( std::optional< GLuint > maybeVertShader = loadShader( "../shaders/texture.vert", GL_VERTEX_SHADER ))
+    vertShader = *maybeVertShader;
+  else
+    return 1;
+  Destroyer _vertShader{ [vertShader] { glDeleteShader( vertShader ); }};
+
+  //------------------------------------------------------------------------------
+
+  GLuint fragShader{};
+  if( std::optional< GLuint > maybeFragShader = loadShader( "../shaders/texture.frag", GL_FRAGMENT_SHADER ))
+    fragShader = *maybeFragShader;
+  else
+    return 1;
+  Destroyer _fragShader{ [fragShader] { glDeleteShader( fragShader ); }};
+
+  //------------------------------------------------------------------------------
+
+  GLuint shaderProgram = glCreateProgram();
+  Destroyer _shaderProgram{ [shaderProgram]{ glDeleteProgram( shaderProgram ); }};
+  glAttachShader( shaderProgram, vertShader );
+  glAttachShader( shaderProgram, fragShader );
+  glLinkProgram( shaderProgram );
+  if( GLint linkStatus; glGetProgramiv( shaderProgram, GL_LINK_STATUS, &linkStatus ), linkStatus == GL_FALSE )
+  {
+    std::cerr << "shader program link failed!\n";
+    return 1;
+  }
+  glUseProgram( shaderProgram );
+
+  //------------------------------------------------------------------------------
+
+  // get error 1282 from glDrawArrays when I don't use any vertex array objects...
+  // shouldn't need any because the vertices are generated in the vert shader, but maybe I need something here...
+  GLuint emptyVertexArray = 0;
+  glGenVertexArrays( 1, &emptyVertexArray );
+  Destroyer _emptyVertexArray{ [emptyVertexArray]{ glDeleteVertexArrays(1, &emptyVertexArray); }};
+  glBindVertexArray( emptyVertexArray );
+
+  //------------------------------------------------------------------------------
+
+  // Event loop
+  while( !glfwWindowShouldClose( window ))
+  {
+    // TODO: wait for signal to draw a frame; maybe wait on a condition variable or something
+
+    // Clear the screen to black
+//            glClearColor( 0.5f, 0.0f, 0.0f, 1.0f );
+//            glClear( GL_COLOR_BUFFER_BIT );
+
+    glDrawArrays( GL_TRIANGLE_STRIP, 0, 4 );
+
+    glfwSwapBuffers( window );
+    glfwPollEvents();
   }
 }
