172afeca55ebf0087bc0176c7e6d1723544e6c95
Atlee Brink <ExclusiveOrange@users.noreply.github.com>
2021-08-25 00:18:58 -0700

    now initially centers the window on the primary monitor

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ namespace
 {
 struct Destroyer
 {
-  const std::function<void( void )> fnOnDestroy;
+  const std::function< void( void ) > fnOnDestroy;
 
   ~Destroyer() { fnOnDestroy(); }
 };
@@ -36,7 +36,7 @@ struct
   RenderThreadState state = RenderThreadState::shouldRender;
   std::mutex mutex;
   std::condition_variable condVar;
-  std::optional<FrameSize> frameSizeUpdate;
+  std::optional< FrameSize > frameSizeUpdate;
 } renderThreadShared;
 
 //------------------------------------------------------------------------------
@@ -63,11 +63,11 @@ void _glfwWindowRefreshCallback( GLFWwindow *window )
 
 //------------------------------------------------------------------------------
 
-std::optional<GLuint>
+std::optional< GLuint >
 loadShader( const char *filename, GLenum shaderType )
 {
   GLuint shader = glCreateShader( shaderType );
-  const std::vector<char> source = readFile( filename );
+  const std::vector< char > source = readFile( filename );
   const GLchar *pShaderSource[] = { source.data() }; // need GLchar**
   const GLint shaderSourceLength[] = { (GLint)source.size() };
   glShaderSource( shader, 1, pShaderSource, shaderSourceLength );
@@ -81,13 +81,38 @@ loadShader( const char *filename, GLenum shaderType )
   GLint logLength = 0;
   glGetShaderiv( shader, GL_INFO_LOG_LENGTH, &logLength );
   // The logLength includes the NULL character
-  std::vector<GLchar> errorLog( logLength );
+  std::vector< GLchar > errorLog( logLength );
   glGetShaderInfoLog( shader, logLength, &logLength, &errorLog[0] );
   std::cout << "bad shader (" << filename << ") " << errorLog.data() << std::endl;
 
   glDeleteShader( shader );
 
   return {};
+}
+
+void centerGlfwWindow( GLFWwindow *window, GLFWmonitor *monitor )
+{
+  struct { int x, y, w, h; } monitorWorkArea;
+  glfwGetMonitorWorkarea( monitor, &monitorWorkArea.x, &monitorWorkArea.y, &monitorWorkArea.w, &monitorWorkArea.h );
+  struct { int x, y; } monitorWorkAreaCenter{
+      monitorWorkArea.w / 2 + monitorWorkArea.x,
+      monitorWorkArea.h / 2 + monitorWorkArea.y };
+
+  struct { int w, h; } windowSize;
+  glfwGetWindowSize( window, &windowSize.w, &windowSize.h );
+
+  struct { int l, t, r, b; } frameSize;
+  glfwGetWindowFrameSize( window, &frameSize.l, &frameSize.t, &frameSize.r, &frameSize.b );
+
+  struct { int w, h; } fullWindowSize{
+      windowSize.w + frameSize.l + frameSize.r,
+      windowSize.h + frameSize.t + frameSize.b };
+
+  struct { int x, y; } windowContentPos{
+      monitorWorkAreaCenter.x - fullWindowSize.w / 2 + frameSize.l,
+      monitorWorkAreaCenter.y - fullWindowSize.h / 2 + frameSize.t };
+
+  glfwSetWindowPos( window, windowContentPos.x, windowContentPos.y );
 }
 } // namespace
 
@@ -131,7 +156,7 @@ int main( int argc, char *argv[] )
 
   //------------------------------------------------------------------------------
 
-  std::optional<Image> maybeImage;
+  std::optional< Image > maybeImage;
   std::thread imageLoadingThread{
       [&imageFilename, &maybeImage]
       {
@@ -159,11 +184,12 @@ int main( int argc, char *argv[] )
   //------------------------------------------------------------------------------
 
   glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
-  glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
+  glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE );
   glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
   glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
-  glfwWindowHint( GLFW_VISIBLE, GL_FALSE );
-  glfwWindowHint( GLFW_TRANSPARENT_FRAMEBUFFER, 1 );
+  glfwWindowHint( GLFW_VISIBLE, GLFW_FALSE );
+  glfwWindowHint( GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE );
+  glfwWindowHint( GLFW_DECORATED, GLFW_TRUE );
 
   GLFWwindow *window = glfwCreateWindow( 640, 480, "My Title", nullptr, nullptr );
   if( !window )
@@ -172,6 +198,16 @@ int main( int argc, char *argv[] )
     return 1;
   }
   Destroyer _glfwCreateWindow{ [window] { glfwDestroyWindow( window ); }};
+
+  //------------------------------------------------------------------------------
+
+  // DELETE: get current monitor work area and print it
+  {
+    int xpos, ypos, width, height;
+    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+    glfwGetMonitorWorkarea( monitor, &xpos, &ypos, &width, &height );
+    std::cout << "monitor work area: {" << xpos << ", " << ypos << ", " << width << ", " << height << std::endl;
+  }
 
   //------------------------------------------------------------------------------
 
@@ -190,7 +226,7 @@ int main( int argc, char *argv[] )
   //------------------------------------------------------------------------------
 
   GLuint vertShader{};
-  if( std::optional<GLuint> maybeVertShader = loadShader( "../shaders/texture.vert", GL_VERTEX_SHADER ))
+  if( std::optional< GLuint > maybeVertShader = loadShader( "../shaders/texture.vert", GL_VERTEX_SHADER ))
     vertShader = *maybeVertShader;
   else
     return 1;
@@ -199,7 +235,7 @@ int main( int argc, char *argv[] )
   //------------------------------------------------------------------------------
 
   GLuint fragShader{};
-  if( std::optional<GLuint> maybeFragShader = loadShader( "../shaders/texture.frag", GL_FRAGMENT_SHADER ))
+  if( std::optional< GLuint > maybeFragShader = loadShader( "../shaders/texture.frag", GL_FRAGMENT_SHADER ))
     fragShader = *maybeFragShader;
   else
     return 1;
@@ -284,6 +320,7 @@ int main( int argc, char *argv[] )
     glfwSetWindowSize( window, scaledWidth, scaledHeight );
   }
   glfwSetWindowAspectRatio( window, image.width, image.height );
+  centerGlfwWindow( window, glfwGetPrimaryMonitor());
 
   //------------------------------------------------------------------------------
 
@@ -315,7 +352,7 @@ int main( int argc, char *argv[] )
           if( renderThreadShared.state == RenderThreadState::shouldQuit )
             break;
 
-          if( std::optional<FrameSize>
+          if( std::optional< FrameSize >
               maybeFrameSize = std::exchange( renderThreadShared.frameSizeUpdate, std::nullopt ))
             glViewport( 0, 0, maybeFrameSize->width, maybeFrameSize->height );
 
